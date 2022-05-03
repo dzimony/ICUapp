4784c2e0d350f69e3dad0813a8f7880de504279f
dzimony <dzimony@gmail.com>
2022-05-03 10:44:23 +0100

    Todays change

--- ICUapp/Shared/Employee.cs
+++ ICUapp/Shared/Employee.cs
@@ -9,6 +9,8 @@ namespace ICUapp.Shared
     internal class Employee
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
     }
 }
